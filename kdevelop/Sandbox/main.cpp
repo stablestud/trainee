ab70d798ac3d705f9615a20bd97c570aaadbef34
stablestud <stablestud@protonmail.com>
2018-02-04 20:09:37 +0100

    Removed build directory

--- kdevelop/Sandbox/main.cpp
+++ kdevelop/Sandbox/main.cpp
@@ -8,12 +8,14 @@ int main ( void )
         int pointer[4] { 1, 6, 2, 9 };
         int *pointer2 = new int [4];
         pointer2 = pointer;
-        cout << * ( &pointer2 + 1 )/* / 4sizeof ( pointer [ 0 ] )*/ << " elements ... " << endl;
-        /*
-        delete[] pointer;
-        pointer = new int [5];
-        pointer = pointer2;
-        pointer[4] = 7;*/
+        cout << sizeof ( &pointer2 + 1 ) / sizeof ( pointer [ 0 ] ) << " elements ... " << endl;
+        delete[] pointer2;
+        pointe2 = new int [5];
+        pointe2 = pointer;
+        pointer2[4] = 7;
         cout << sizeof ( pointer ) / sizeof ( pointer [ 0 ] ) << " elements ... " << endl;
+        int array[3];
+        cout << sizeof ( array ) << " " << sizeof ( array[0] ) << endl;
+        delete[] pointer2;
         return 0;
 }

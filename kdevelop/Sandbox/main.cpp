e4c8eb979e2a5a16f29684f4925dcb5df5192ffc
stablestud <stablestud@protonmail.com>
2018-02-11 19:42:34 +0100

    Updated Sandbox with two-dimensial pointer arrays

--- kdevelop/Sandbox/main.cpp
+++ kdevelop/Sandbox/main.cpp
@@ -4,8 +4,25 @@ using namespace std;
 
 int main ( void )
 {
-        int valid[6] { 1,1,2,3,4,5 };
-        int *aux = valid;
-        cout << aux[0] << endl;
+        int rows = 5;
+        int **arr;
+        // allocate and initialize the array
+        arr = new int * [rows];
+        for ( int r = 0; r < rows; r++ ) {
+                arr[r] = new int[r + 1];
+                for ( int c = 0; c <= r; c++ )
+                        arr[r][c] = ( r + 1 ) * 10 + c + 1;
+        }
+        // print the array
+        for ( int r = 0; r < rows; r++ ) {
+                for ( int c = 0; c <= r; c++ )
+                        cout << arr[r][c] << " ";
+                cout << endl;
+        }
+        // free the array
+        for ( int r = 0; r < rows; r++ )
+                delete [] arr[r];
+        delete [] arr;
         return 0;
 }
+

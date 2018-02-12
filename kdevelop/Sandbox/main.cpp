f0218d81f0e6366ba96e6e7000423e71d1f38222
stablestud <stablestud@protonmail.com>
2018-02-12 23:03:47 +0100

    Updated kdevelop/Sandbox with multi-dimensial pointer arry

--- kdevelop/Sandbox/main.cpp
+++ kdevelop/Sandbox/main.cpp
@@ -4,25 +4,12 @@ using namespace std;
 
 int main ( void )
 {
-        int rows = 5;
-        int **arr;
-        // allocate and initialize the array
-        arr = new int * [rows];
-        for ( int r = 0; r < rows; r++ ) {
-                arr[r] = new int[r + 1];
-                for ( int c = 0; c <= r; c++ )
-                        arr[r][c] = ( r + 1 ) * 10 + c + 1;
-        }
-        // print the array
-        for ( int r = 0; r < rows; r++ ) {
-                for ( int c = 0; c <= r; c++ )
-                        cout << arr[r][c] << " ";
-                cout << endl;
-        }
-        // free the array
-        for ( int r = 0; r < rows; r++ )
-                delete [] arr[r];
-        delete [] arr;
+        int ***arr = new int **[4] { new int *[4] { new int [4] { 1, 2, 3, 4 }, new int [4] { 5, 6, 7, 8 }, new int [4] { 9, 10, 11, 12 }, new int [4]  { 13, 14, 15, 16 } }, new int *[4] { new int [4] { 17, 18, 19, 20 }, new int [4] { 21, 22, 23, 24 }, new int [4] { 25, 26, 27, 28 }, new int [4]  { 29, 30, 31, 32 } }, new int *[4] { new int [4] { 33, 34, 35, 36 }, new int [4] { 37, 38, 39, 40 }, new int [4] { 41, 42, 43, 44 }, new int [4]  { 45, 46, 47, 48 } }, new int *[4] { new int [4] { 49, 50, 51, 52 }, new int [4] { 53, 54, 55, 56 }, new int [4] { 57, 58, 59, 60 }, new int [4]  { 61, 62, 63, 64 } } };
+
+        cout << arr[3][1][3] << endl;;
+
         return 0;
 }
+
+
 

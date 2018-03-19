88ec9fef2329e0a2028e5a3a2d7de641c47ed6d4
stablestud <stablestud@protonmail.com>
2018-03-19 15:57:28 +0100

    Added lab pdfs

--- kdevelop/Sandbox/Sandbox/main.cpp
+++ kdevelop/Sandbox/Sandbox/main.cpp
@@ -2,58 +2,7 @@
 
 using namespace std;
 
-class ClassSupreme
-{
-protected:
-        static int level;
-public:
-        ClassSupreme ( void )
-        {
-                cout << "Initializing ClassSupreme..." << endl;
-        }
-
-        ~ClassSupreme ( void )
-        {
-                cout << "Killing ClassSupreme..." << endl;
-        }
-
-};
-
-class SubClassSupreme : ClassSupreme
- {
- public:
-        SubClassSupreme ( void )
-        {
-                cout << "Initializing SubClassSupreme... total level: " << ClassSupreme::level++ << endl;
-        }
-
-        ~SubClassSupreme ( void )
-        {
-                cout << "Killing SubClassSupreme..." << endl;
-        }
-};
-
-class SubSubClassSupreme : SubClassSupreme
-{
-public:
-        SubSubClassSupreme ( void )
-        {
-                cout << "Initializing SubSubClassSupreme... total level: " << endl;
-        }
-
-        ~SubSubClassSupreme ( void )
-        {
-                cout << "Killing SubSubClassSupreme..." << endl;
-        }
-};
-
-int ClassSupreme::level = 0;
-
 int main ( void )
 {
-        ClassSupreme A;
-        SubClassSupreme b;
-        SubSubClassSupreme a;
-
         return 0;
 }

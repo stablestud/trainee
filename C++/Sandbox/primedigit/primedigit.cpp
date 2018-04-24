803cbe2a8949b08de39d88ddb854731e87c3c142
stablestud <stablestud@protonmail.com>
2018-04-24 10:42:26 +0200

    Finished primedigit

--- C++/Sandbox/primedigit/primedigit.cpp
+++ C++/Sandbox/primedigit/primedigit.cpp
@@ -1,45 +1,31 @@
 #include <iostream>
 
-struct PRIMZAHL {
-	int zahl;
-	PRIMZAHL* next;
-};
-
-void printPrimzahl ( PRIMZAHL** current );
+/** 
+ * @author stablestud
+ * @date 24.04.2018
+ */
 
 int main ( void )
 {
-	PRIMZAHL* container
-       	PRIMZAHL** current = &container;	
-
 	for ( int i = 0; i <= 100; i++ ) {
+		/* Sets the maximum possible divider */
 		int bTeiler = i / 2;
-		int j = i;
+		int j = 2;
+		bool primedigit = true;
 
 		do {
-			if ( !( i % j )) {
-				(*current) = new PRIMZAHL { i, nullptr };
-				(*current) = (*current)->next;
+			/* When i Modulo j equals to 0, then it's a primedigit.
+			 * Exception is the 2 */	
+			if ( !( i % j ) && i != 2 ) {
+				primedigit = false;
 				break;
 			}
-
+		/* Aslong the maximum possible divider has not been reached
+		 * run the loop */
 		} while ( j++ <= bTeiler );
+
+		if ( primedigit )
+			std::cout << "PRIMEDIGIT: " << i << std::endl;
 	}
-
-	printPrimzahl ( &container );
 }
 
-void printPrimzahl ( PRIMZAHL* current )
-{
-	if ( current == nullptr )
-		return;
-
-	if ( current == nullptr )
-		return;
-
-	static int count = 1;
-
-	std::cout << "Primzahl [" << count++ << "]: " << current->zahl << std::endl;
-
-	printPrimzahl ( current->next );
-}

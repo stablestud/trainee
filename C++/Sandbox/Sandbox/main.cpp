823f62486092c99b779d7f7d2fdfd16044b37731
stablestud <stablestud@protonmail.com>
2018-04-28 23:19:29 +0200

    Update try.txt

--- C++/Sandbox/Sandbox/main.cpp
+++ C++/Sandbox/Sandbox/main.cpp
@@ -1,52 +1,23 @@
 #include <iostream>
+using namespace std;
 
-class BASE
-{
-public:
-        virtual void func ( void )
-        {
-                std::cout << "It func" << std::endl;
-        }
-};
-
-class DERIVED : public BASE
-{
-public:
-        void func ( void )
-        {
-                std::cout << "It func deeper" << std::endl;
-        }
-
-        void specialo ( void )
-        {
-                func();
-                std::cout << "|| upper is mine " << std::endl;
-        }
-};
-
-void func ( BASE& );
-
-int main ( void )
-{
-        BASE ok_cool;
-        DERIVED gang;
-        
-        func ( ok_cool );
-        std::cout << "..................." << std::endl;
-        func ( gang );
-
-        return 0;
+namespace S {
+        int A = 1;
 }
 
-void func ( BASE& base )
-{
-        try {
-                dynamic_cast<DERIVED&>(base).specialo();
-        
-        } catch (...) {};
+namespace S {
+        int B = A + 2 ;
+}
 
-        try {
-                dynamic_cast<BASE&>(base).func();
+int main(void) {
+        S::A = S::A + 1;
 
-        } catch (...) {};
+        {
+                using namespace S;
+                ++B;
+        }
+
+        cout << S::B << S::A;
+
+        return 0;
 }

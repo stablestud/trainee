cce12517d57354b93e48550078565a71577cc710
stablestud <stablestud@protonmail.com>
2018-05-06 12:22:59 +0200

    Fixed sign in cpa_lab_5_3_10__5

--- C++/cisco/cpa_lab/cpa_lab_5_3_10__5/fraction.cpp
+++ C++/cisco/cpa_lab/cpa_lab_5_3_10__5/fraction.cpp
@@ -13,7 +13,7 @@ std::string Fraction::toString ( void )
         int prefix = numerator / denominator;
         std::string sprefix = ( prefix ) ? std::to_string ( prefix ) + " " : "";
         int suffix = numerator % denominator; 
-        std::string ssuffix = ( suffix ) ? std::to_string ( suffix ) + "/" + std::to_string ( denominator ) + " " : "";
+        std::string ssuffix = ( suffix ) ? std::to_string ( !prefix ? ( denominator < 0 ? ~suffix + 1 : suffix ) : ( numerator < 0 ? ~suffix + 1 : suffix )) + "/" + std::to_string (  denominator < 0 ? ~denominator + 1 : denominator ) + " " : "";
 
 	return sprefix + ssuffix + "is " + std::to_string ( this -> toDouble() ) + " in decimal.";
 }

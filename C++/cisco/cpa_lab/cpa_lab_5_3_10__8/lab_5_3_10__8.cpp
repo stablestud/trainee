327457d38b54a937070ba346e2ab272fcf576370
stablestud <stablestud@protonmail.com>
2018-05-13 11:28:25 +0200

    Completed cleanString() in cpa_lab_5_3_10__8

--- C++/cisco/cpa_lab/cpa_lab_5_3_10__8/lab_5_3_10__8.cpp
+++ C++/cisco/cpa_lab/cpa_lab_5_3_10__8/lab_5_3_10__8.cpp
@@ -2,9 +2,17 @@
 #include <iostream>
 #include "Point2D.h"
 
+std::string cleanString ( std::string input );
+
 int main ( void )
 {
-	
+	std::string input;
+	std::getline ( std::cin, input );	
+
+	input = cleanString ( input );
+
+	std::cout << input << std::endl;
+
 	return 0;
 }
 
@@ -12,4 +20,20 @@ int main ( void )
 void readInput ( void )
 {
 	
+}
+
+std::string cleanString ( std::string input )
+{
+	for ( unsigned i = 0; i < input.length(); i++ )
+		if ( input[i] < '0' || input[i] > '9' )
+			input[i] = ' ';
+			
+	for ( unsigned i = input.length(); i > 0; i-- )
+		if ( input[i] == ' ' && input[i - 1] == ' ' && i != 0 )
+			input.erase ( i, 1 );
+
+	for (; input[0] == ' ';)
+		input.erase ( 0, 1 );
+
+	return input;
 }

c45e39b14187238874948292a7125240a197aa61
stablestud <stablestud@protonmail.com>
2018-05-13 02:02:33 +0200

    Started cpa_lab_5_3_10__8

---
+++ C++/cisco/cpa_lab/cpa_lab_5_3_10__8/Point2D.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <cmath>
+#include "Point2D.h"
+
+Point2D::Point2D ( double x, double y )
+{
+	this -> x = x;
+	this -> y = y;
+}
+
+std::string Point2D::toString ( void )
+{
+	return "";
+}
+
+double Point2D::toDouble ( void )
+{
+	return 0.;
+}
+
+double Point2D::distanceTo ( Point2D that )
+{
+	/* Calculate the length of each side */
+	int y = this -> y - that.returnY();
+	int x = this -> x - that.returnX();
+
+	return std::sqrt ( y * y + x * x );
+}
+
+double Point2D::returnX ( void )
+{
+	return this -> x;
+}
+
+double Point2D::returnY ( void )
+{
+	return this -> y;
+}
---
+++ C++/cisco/cpa_lab/cpa_lab_5_3_10__8/lab_5_3_10__8.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+#include "Point2D.h"
+
+int main ( void )
+{
+	
+	return 0;
+}
+
+/* Read & filter input, allow to be delimetered by comma and space */
+void readInput ( void )
+{
+	
+}

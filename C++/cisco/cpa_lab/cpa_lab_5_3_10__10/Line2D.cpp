c5d780b654f18cf3b4e98e532fba3c740e4275b4
Vladislav <stablestud@protonmail.com>
2018-05-17 07:08:17 +0200

    Remove unused libraries in cpa_5_3_10__10 Line2D.cpp

--- C++/cisco/cpa_lab/cpa_lab_5_3_10__10/Line2D.cpp
+++ C++/cisco/cpa_lab/cpa_lab_5_3_10__10/Line2D.cpp
@@ -1,6 +1,4 @@
 #include <string>
-#include <iostream>
-#include <cmath>
 #include "Point2D.h"
 #include "Line2D.h"
 
@@ -8,7 +6,7 @@ Line2D::Line2D ( Point2D pointA, Point2D pointB )
 {
 	/* Calculate the slope and y interception ... */
 	this -> slope = ( pointB.getY() - pointA.getY()) / ( pointB.getX() - pointA.getX());
-	y_intercept = pointA.getY() - slope * pointA.getX();
+	y_intercept = pointA.getY() - this -> slope * pointA.getX();
 }
 
 Line2D::Line2D ( double slope, double y_intercept )

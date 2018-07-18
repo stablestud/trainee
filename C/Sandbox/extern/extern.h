d2df5dc0815cc85ae3600cbb38a06435140eb7aa
stablestud <stablestud@protonmail.com>
2018-07-18 20:31:44 +0200

    Updated description in extern.h

--- C/Sandbox/extern/extern.h
+++ C/Sandbox/extern/extern.h
@@ -6,6 +6,8 @@
 // for variables it'll be forward declaration if its preceeded by a extern keyword and/or when
 // the variable declaration is not in the same statement initialized but it must be defined (with initializer) somewhere else,
 // if its not defined somewhere else, a variables without the extern keyword will be taken as definition.
+// Also a variabled declaration with a preceeding "extern" and with initialization will be taken as variable definition,
+// rendering the use of the "extern" keyword useless
 extern void func(char*);
 extern char* storage;
 

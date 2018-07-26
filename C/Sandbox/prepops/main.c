eb19eb1e3e940aac51ecfcd3b9a81e4f555eb802
stablestud <stablestud@protonmail.com>
2018-07-26 14:57:40 +0200

    Tested few directives

--- C/Sandbox/prepops/main.c
+++ C/Sandbox/prepops/main.c
@@ -1,11 +1,31 @@
-#include <stdio.h>
 
-#define QUOTE(X) #X
+#define QUOTE(X) (#X)
 #define GLUE(X,Y) QUOTE(X##Y) /* I can't do #X##Y compiler will throw an error */
-#define P(X) puts(X);
+
+#if defined __cplusplus
+#       include <iostream>
+#       define PRINT(X) std::cout << #X << std::endl;
+#       define COMPILER 2
+#       define COMPL    CPP
+#else
+#       include <stdio.h>
+#       define PRINT(X) puts(#X);
+#       define COMPILER 1
+#       define COMPL    C
+#endif
+
+#if (COMPILER == 2)
+#       warning CPP
+#elif (COMPILER == 1)
+#       warning C
+#else
+#       warning OTHER
+#endif
+
+#define P(X) PRINT(X)
 
 int main(int argc, char* argv[])
 {
-	P(GLUE(Hello World, Mister))
+	P(COMPL)
 	return 0;
 }

74c8887c501b84aa40a8b8791645e4896c4b7698
stablestud <stablestud@protonmail.com>
2018-07-27 13:43:13 +0200

    Added debugging compilation to Makefile

--- C/Sandbox/Sandbox/main.c
+++ C/Sandbox/Sandbox/main.c
@@ -1,28 +1,7 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdint.h>
-#include <errno.h>
-
-
-double calc_circle_area(double radius, double unused1)
-{
-	return(3.14*radius*radius);
-}
-
-calc_any_area(double (*pFunc)(double, double))
-{
-	double result = (*pFunc)(12.4, 0);     // call using function pointer
-	return(result);
-}
-
-void calcArea()
-{
-	double (*calc_area)(double, double)=&calc_circle_area;
-	calc_any_area(&calc_area);
-}
+#include <stdbool.h>
 
 int main(void)
 {
-	return 0;
+	bool test = 1;
+	return sizeof(BOOL_MAX);
 }

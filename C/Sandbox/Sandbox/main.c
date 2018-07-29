9de1ed66cc37b1882cde86e9c49c428c21492c3d
stablestud <stablestud@protonmail.com>
2018-07-29 12:08:18 +0200

    Added assert to C/Sandbox

--- C/Sandbox/Sandbox/main.c
+++ C/Sandbox/Sandbox/main.c
@@ -1,7 +1,18 @@
+#include <assert.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <stdbool.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <stdalign.h>
 
 int main(void)
 {
-	bool test = 1;
-	return sizeof(BOOL_MAX);
+	int iVar = 5;
+	int aVar = iVar + iVar++;
+	printf("%d\n", aVar);
+
+	return 0;
 }

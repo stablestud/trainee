5801517c2d71ae94afc1844340f883941359aa7a
stablestud <stablestud@protonmail.com>
2018-09-05 08:39:32 +0200

    Finished a few labs in CLA
    cla_lab_2_5_17__1
    cla_lab_2_5_17__2
    cla_lab_2_5_17__3

--- C/cisco/CLA/lab/cla_lab_2_3_19__3/main.c
+++ C/cisco/CLA/lab/cla_lab_2_3_19__3/main.c
@@ -5,10 +5,10 @@ int main()
 	char zero = '0';
 
 	/*Your code*/
-	for (int i = 1; i <= 9; i++)
-		printf("'%d' - '%c' is: %d\n", i, zero, i - 0);
+	for (char i = '1'; i <= '9'; i++)
+		printf("'%c' - '%c' is: %d\n", i, zero, i - '0');
 
-	printf("'%d' - '%c' is: %d\n", 0, zero, 0 - 0);
+	printf("'%c' - '%c' is: %c\n", zero, zero, zero);
 	return 0;
 }
 

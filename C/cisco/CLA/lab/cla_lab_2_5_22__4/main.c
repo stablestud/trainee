d2e61f981fa5793971d8366a01f5b0981f1035e4
stablestud <stablestud@protonmail.com>
2018-09-05 20:17:22 +0200

    Finished cla_lab_2_5_22_{1..4}

---
+++ C/cisco/CLA/lab/cla_lab_2_5_22__4/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int day;
+	char *sday;
+
+	printf("Please input the weekdays number: ");
+	scanf("%d", &day);
+
+	if (day & 7) {
+		char *csday[7] = { 
+			"Monday",
+			"Tuesday",
+			"Wednesday",
+			"Thursday",
+			"Friday",
+			"Saturday",
+			"Sunday"
+		};
+
+		printf("The day is: %s\n", csday[day - 1]);
+	} else {
+		printf("No such day with number %d, valid range from 1 to 7!\n", day);
+	}
+}	

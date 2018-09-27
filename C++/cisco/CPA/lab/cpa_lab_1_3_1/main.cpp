b2b80a6857ec0374b8e01187957cc07526750595
stablestud <stablestud@protonmail.com>
2018-09-27 18:43:56 +0200

    Finished a few cpa_labs

---
+++ C++/cisco/CPA/lab/cpa_lab_1_3_1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>//we included iostream
+#include <iomanip>//we included iomanip
+#include <string>//we included string
+
+int main()
+{
+	int v=10800; // 3*60*60
+	int z2z=3*60;// This is a variable to hold the value of 3 minutes in seconds
+	int zzz=5*60;// This is a variable to hold the value of 6 minutes in seconds
+	float siii=3.141526; //This is the value of pi
+	//std::cout << "result: " << result << std::endl;
+	// here we should print the v value but a programmer didn't have time to write any code
+	// but he/she likes writing long comments
+}
+

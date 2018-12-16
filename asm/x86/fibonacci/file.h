e59c08378a730e198bdb7568bf2d6448095666b3
stablestud <stablestud@protonmail.com>
2018-12-16 14:40:05 +0100

    Added funload function to asm/fibonacci

--- asm/x86/fibonacci/file.h
+++ asm/x86/fibonacci/file.h
@@ -6,9 +6,18 @@
 	%rdi - address of null terminated string containing file path 
 
 	Success: return starting address of mapped file
-	Fail:	 prints error message on screen and returns -1 (64 bit)
+	Failure: prints error message on screen and returns -1 (64 bit)
 */
-
 .extern fload
+
+/* Unmaps file and close file decriptor
+	
+	%rdi - address to mapped memory
+	%rsi - file descriptor to close
+
+	Success: return zero
+	Failure: prints error message and return -1 or -2 (64bit)
+*/
+.extern funload
 
 #endif /* FILE_H */
